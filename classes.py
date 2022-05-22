0cdaf623fb729a27b0e6072b99fd3718849a9999
emacc <lalitoceja@gmail.com>
2022-05-22 12:53:13 -0500

    models

---
+++ classes.py
@@ -0,0 +1,25 @@
+from typing import List
+from typing_extensions import Self
+
+class centroide():
+    def __init__(self,x :float,  y: float) -> Self:
+        self.__x = x
+        self.__y = y
+        self.puntos = []
+        self.capacidades = []
+        self.__distancias = []
+
+    def __repr__(self) -> str:
+        return f"(x = {self.x}, y = {self.y})"
+
+    @property
+    def x(self) -> float:
+        return self.__x
+
+    @property
+    def y(self) -> float:
+        return self.__y
+
+    @property
+    def distancias(self) -> List[float]:
+        return self.__distancias
