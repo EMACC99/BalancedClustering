c94c707131b570738cfe43b57ae1ac093d8b33b9
emacc <lalitoceja@gmail.com>
2022-05-23 13:48:10 -0500

    cython flags

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 from Cython.Build import cythonize
+import numpy
 
 setup(
-    ext_modules = cythonize("funciones_paralelas.pyx"),
+    ext_modules = cythonize("funciones_paralelas.pyx", gdb_debug = True, language_level = "3"),
+    include_dirs=[numpy.get_include()],
 )
