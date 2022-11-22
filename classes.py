3b89baa59962961565aeeb2c77475c98f5336951
emacc <lalitoceja@gmail.com>
2022-11-22 17:57:15 -0600

    equal methods

--- classes.py
+++ classes.py
@@ -3,15 +3,20 @@ from typing_extensions import Self
 
 
 class centroide:
-    def __init__(self, x: float, y: float) -> Self:
+    def __init__(self, x: float, y: float) -> None:
         self.__x = x
         self.__y = y
-        self.puntos = []
-        self.capacidades = []
-        self.__distancias = []
+        self.capacidades: List[int] = []
+        self.puntos: List[tuple] = []
 
     def __repr__(self) -> str:
         return f"(x = {self.x}, y = {self.y})"
+
+    def __eq__(self, other: Self) -> bool:
+        return self.__x == other.__x and self.__y == other.__y
+
+    def __ne__(self, other: object) -> bool:
+        return not self.__eq__(self, other)
 
     @property
     def x(self) -> float:
@@ -20,7 +25,3 @@ class centroide:
     @property
     def y(self) -> float:
         return self.__y
-
-    @property
-    def distancias(self) -> List[float]:
-        return self.__distancias
