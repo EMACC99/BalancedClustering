4b66dfffd6f188db0fa885a5e711506bf2e9dc62
Eduardo Ceja <lalitoceja@gmail.com>
2022-11-28 20:17:57 -0600

    removed debug info and enable profiling

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
 from setuptools import setup
 from Cython.Build import cythonize
 import numpy
+import multiprocessing
 
 setup(
     ext_modules=cythonize(
-        "funciones_paralelas.pyx", gdb_debug=True, language_level="3"
+        "funciones_paralelas.pyx", gdb_debug=False, language_level="3", nthreads=-1,  compiler_directives={"profile" : True}
     ),
     include_dirs=[numpy.get_include()],
 )
